2a2a7a36b89f101fb3cef0d8d9080eabb1b986ff
markddrake <mark.drake@oracle.com>
2015-05-11 22:27:11 -0700

    Fix Uber Install Scripts

--- scripts/installGitHubRepositories.sh
+++ scripts/installGitHubRepositories.sh
@@ -2,13 +2,14 @@ DBA=$1
 DBAPWD=$2
 XFILES=$3
 XFILESPWD=$4
-DEMOUSER=$5
-DEMOPWD=$6
-SERVER=$7
-HTTPPORT=$8
-XDBEXT=$9
-XDBEXTPWD=${10}
-SERVERURL="$SERVER:$HTTPPORT"
+XDBEXT=$5
+XDBEXTPWD=$6
+DEMOUSER=$7
+DEMOPWD=$8
+HOSTNAME=$9
+HTTPPORT=${10}
+SERVERURL="$HOSTNAME:$HTTPPORT"
+INSTALLROOT=`pwd`
 rm -rf oracle-xml-sample-demo-*
 curl -Lk https://github.com/oracle/xml-sample-demo/zipball/master -o xml-sample-demo.zip
 unzip -o xml-sample-demo.zip
@@ -16,8 +17,9 @@ cd oracle-xml-sample-demo*
 sqlplus $DBA/$DBAPWD @scripts/installXFiles $XFILES $XFILESPWD $HTTPPORT
 sqlplus $DBA/$DBAPWD @scripts/installMetadata $XDBEXT $XDBEXTPWD
 sh scripts/installXMLRepository.sh $DBA $DBAPWD $XFILES $XFILESPWD $XDBEXT $XDBEXTPWD $DEMOUSER $DEMOPWD $SERVERURL
-curl -Lk https://github.com/oracle/json-in-db/zipball/master -o json-in-db.zip
 rm -rf oracle-json-in-db-*
+cd $INSTALLROOT
+curl -Lk https://github.com/oracle/json-in-db/zipball/master -o json-in-db.zip
 unzip -o json-in-db.zip
 cd oracle-json-in-db*
 sh scripts/installJSONRepository.sh $DBA $DBAPWD $XFILES $XFILESPWD $XDBEXT $XDBEXTPWD $DEMOUSER $DEMOPWD $SERVERURL
